787b281f691bfe5c7fa1d48e584bd385b13f0af6
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-09-21 21:33:07 +0100

    aligned text

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import background from "./books.jpg";
 
 export default function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${background})`, minHeight: '100%'}} >
+    <div className="App" style={{ backgroundImage: `url(${background})`, height: '100%'}} >
       <header className="App-header">
        Hello
       </header>
+    
     </div>
   );
 }
