5bc2afdf7f90156590c56e340982c06b0d3a1aa7
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-09-22 02:54:27 +0100

    added dictionary component and form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
 import background from "./books.jpg";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${background})`, height: '100%'}} >
-      <header className="App-header">
-       Hello
+     <div className="container"> 
+     <header className="App-header">
+       <Dictionary/>
       </header>
-    
+    </div>
     </div>
   );
 }
