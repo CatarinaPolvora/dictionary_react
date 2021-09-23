81c5ed35a133170a6a9efdb5380441fbcb52f090
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-09-23 08:55:10 +0100

    added background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import background from "./books.jpg";
+
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${background})`, height: '100%'}} >
+    <div className="App" >
      <div className="container"> 
      <header className="App-header">
        <Dictionary/>
