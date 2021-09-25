f236fafffda6a68558351f3d8c5ce663180934f0
CatarinaPolvora <82556803+CatarinaPolvora@users.noreply.github.com>
2021-09-26 00:36:27 +0100

    added results and meaning components; added box on css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary () {
-    let [keyword, setKeyword] =useState("");
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
     
     function handleResponse(response){
-console.log(response.data[0]);
+setResults(response.data[0]);
     }
 
     function search(event){
@@ -20,5 +22,6 @@ axios.get(apiUrl).then(handleResponse);}
     }
     return <div className="Dictionary">
         <form onSubmit={search}><input type="search" onChange={handleKeywordChange} autoFocus={true}/></form>
+        <Results results={results}/>
     </div>
 }
